d8a45cf2b5907c6fbe6eb3bfaeee17e1fd32c7fd
Malcolm <sciand6@gmail.com>
2023-02-26 10:10:29 -0500

    Changed NBA apis.

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,8 +1,9 @@
 from flask import Flask, jsonify
-from nba_api.stats.endpoints import leaguedashteamstats
+from nbapy import league
 import json
 import pandas as pd
 import os
+import math
 
 def df_to_json(df):
     columns = df.columns.to_list()
@@ -24,9 +25,13 @@ def index():
 
 @app.route('/data')
 def data():
-    df = leaguedashteamstats.LeagueDashTeamStats()
-    df = df.get_data_frames()[0]
-    cols = ['TEAM_NAME', 'W_PCT', 'PTS', 'AST', 'REB', 'PTS_RANK', 'AST_RANK', 'REB_RANK']
+    cols = ['TEAM_NAME', 'W_PCT', 'P_EXP', 'PTS', 'OPP_PTS', 'AST', 'REB', 'PTS_RANK', 'OPP_PTS_RANK', 'AST_RANK', 'REB_RANK']
+    team_df = league.TeamStats(per_mode='PerGame').stats()
+    opp_df = league.TeamStats(measure_type='Opponent', per_mode='PerGame').stats()
+    df = pd.merge(team_df, opp_df, on='TEAM_ID')
+    df = df.filter(regex='^(?!.*_y)', axis=1)
+    df = df.rename(columns=lambda x: x.replace('_x', ''))
+    df['P_EXP'] = df.apply(lambda row: math.pow(row['PTS'], 16) / (math.pow(row['PTS'], 16) + math.pow(row['OPP_PTS'], 16)), axis=1).round(2)
     df = df[cols]
     return df_to_json(df), 200
 
